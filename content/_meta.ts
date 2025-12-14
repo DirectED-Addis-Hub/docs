8338769ba892e5941253d7cb011fd245899a5c31
Nahom Behailu Geremew <nahombehailu22@gmail.com>
2025-12-14 22:16:42 +0300

    Order test

---
+++ content/_meta.ts
@@ -0,0 +1,13 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  index: "Home",
+  "our-story": "Our Story",
+  events: "Events",
+  workshops: "Workshops",
+  teams: "Teams",
+  "submission-guidelines": "Submission Guidelines",
+  processes: "Processes"
+};
+
+export default meta;
